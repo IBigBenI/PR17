c2e604ed2a66ccb89326361f58be61cb164f8d1b
Даниил Творогов <tvorogov.dan9@mail.ru>
2022-04-18 22:14:30 +0300

    Добавьте файлы проекта.

---
+++ PR17/PR17/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace PR17
+{
+    public partial class MainPage : ContentPage
+    {    
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void RequestClick(object sender, EventArgs e)
+        {
+            string url = "http://numbersapi.com";
+            RequestLabel.Text = "";
+            using (var WebClient = new WebClient())
+            {
+                string value = Convert.ToBase64String(Encoding.ASCII.GetBytes(RequestEntry.Text));
+                WebClient.Headers.Add("Request", value);
+                var response = WebClient.DownloadString(url);
+                RequestLabel.Text = Convert.ToString(response);
+            }
+        }
+    }
+}
